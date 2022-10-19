17d30ae05ec0c9e21db44e7c07ffa298cd54e2d1
Charles Silva <emaildocharles@gmail.com>
2022-10-19 00:36:25 -0300

    trainning_OO_Lists_Foreach_For_LambidaExpressions

---
+++ Employee.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto2
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void increaseSalary(double percentage)
+        {
+            Salary += Salary * (percentage/100);
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Name + ", " + Salary;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Projeto2;
+
+List<Employee> employee = new List<Employee>();
+
+Console.Write("How many employees will be registered? ");
+int Count = int.Parse(Console.ReadLine());
+int Id = 0;
+string Name = "a";
+double Salary = 0.0;
+
+for (int i = 0; i < Count; i++)
+{
+    System.Console.WriteLine($"Employee #{i + 1}:");
+    System.Console.Write("Id: ");
+    Id = int.Parse(Console.ReadLine());
+    System.Console.Write("Name: ");
+    Name = Console.ReadLine();
+    System.Console.Write("Salary: ");
+
+    employee.Add(new Employee(Id, Name, Salary));
+
+    System.Console.WriteLine();
+}
+
+System.Console.Write("Enter the employee id that will have salary increase: ");
+Id = int.Parse(Console.ReadLine());
+double percentage = 0.0;
+System.Console.Write("Enter the percentage: ");
+percentage = double.Parse(Console.ReadLine());
+
+int indexEmployee = employee.FindIndex(x => x.Id == Id);
+
+employee[indexEmployee].increaseSalary(percentage);
+
+System.Console.WriteLine();
+System.Console.WriteLine("Updated list of employees: ");
+
+foreach (object item in employee)
+{
+    System.Console.WriteLine(item);
+}
+
+
+
+
+
